8b1a11172b6b5543f1886abfec12f7b3f8980197
Joel <joelbearn@gmail.com>
2024-04-24 11:10:08 +0100

    fix: change clerk client dependency

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-// import { clerkClient } from '@clerk/nextjs'
+import { clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
@@ -68,13 +68,13 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
-    // if(newUser) {
-    //   await clerkClient.users.updateUserMetadata(id, {
-    //     publicMetadata: {
-    //       userId: newUser._id
-    //     }
-    //   })
-    // }
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
 
     return NextResponse.json({ message: 'OK', user: newUser })
   }
